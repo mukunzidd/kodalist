1fe26e732e47f066bef67642905ed90146fbcdbd
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-02-20 15:55:20 +0200

    Added reply functionality on posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 	end
 
 	def show
+		@comments
 	end
 
 	def new
---
+++ app/controllers/replies_controller.rb
@@ -0,0 +1,47 @@
+class RepliesController < ApplicationController
+	before_action :find_post
+	def create
+
+		@reply = @post.replies.create(reply_params)
+		@reply.user_id = current_user.id if current_user
+		@reply.save
+
+		if @reply.save
+			redirect_to post_path(@post)
+		else
+			render 'new'
+		end
+
+	end
+
+	def edit
+		@reply = @post.replies.find(params[:id])
+	end
+
+	def update
+		@reply = @post.replies.find(params[:id])
+
+		if @reply.update(reply_params)
+			redirect_to post_path(@post)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@reply = @post.replies.find(params[:id])
+		@reply.destroy
+		redirect_to post_path(@post)
+	end
+
+	private
+
+		def reply_params
+			params.require(:reply).permit(:body)			
+		end
+
+		def find_post
+			@post = Post.find(params[:post_id])		
+		end
+		
+end
