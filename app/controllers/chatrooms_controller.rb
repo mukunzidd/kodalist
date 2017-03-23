1525aaa07c8de764880c3fa70fc7dad19812e80b
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-03-24 00:44:34 +0200

    Added chatroom model and controller

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,17 @@
+class ChatroomsController < ApplicationController
+	def index
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def method_name
+		
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
 	has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
 	has_many :personal_messages, dependent: :destroy
+	has_many :chatrooms, dependent: :destroy	
 
   def username
     email.split('@')[0].capitalize
